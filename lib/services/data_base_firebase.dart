4fde3ce0dc885040b482870ce6b79fb458646a82
Fabian <fabian.nfgv2@gmail.com>
2023-06-14 11:47:00 -0400

    solicitud get en firebase store

--- lib/services/data_base_firebase.dart
+++ lib/services/data_base_firebase.dart
@@ -1,1 +1,16 @@
-class FireBaseStore {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getComida() async {
+  List comidaDB = [];
+
+  CollectionReference collectionReferenceComida = db.collection("comidas");
+
+  QuerySnapshot queryComida = await collectionReferenceComida.get();
+  queryComida.docs.forEach((element) {
+    comidaDB.add(element.data());
+  });
+
+  return comidaDB;
+}
